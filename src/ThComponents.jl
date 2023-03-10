5781e9d3fee4b97b4e97c42a0bd4f327de913e9f
Brad Erickson <bbwe24@gmail.com>
2023-03-09 21:50:48 -0500

    Updates to 0.0.2

--- src/ThComponents.jl
+++ src/ThComponents.jl
@@ -3,7 +3,7 @@ module ThComponents
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.1"
+const version = "0.0.2"
 
 include("jl/editabletable.jl")
 include("jl/roundinforow.jl")
